f90db11caeba539f94ac141851ea808839c61d7b
yantao <yan.tao.he2881@gmail.com>
2022-11-23 09:03:27 -0500

    feat: task 3

--- components/Task3.js
+++ components/Task3.js
@@ -1,26 +1,117 @@
-export default function Task2(props) {
+import React, { useState } from "react";
+import RGL, { WidthProvider, Responsive } from "react-grid-layout";
+import _ from "lodash";
+import "/node_modules/react-grid-layout/css/styles.css";
+import "/node_modules/react-resizable/css/styles.css";
 
-   // TO-DO: When the user clicks Add Item, you should pick
-   // one of these sizes randomly and add to grid.
-   const SIZE_OPTIONS = [
-      [50, 50],
-      [50, 100],
-      [100, 50],
-      [100, 100],
-      [100, 150]
-   ] // in pixels [w, h]
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const ReactGridLayout = WidthProvider(RGL);
 
-   const DESIGN_DETAILS = {
-      sideBarWidth: 300, // in pixels
-      numberOfInitialBoxes: 69,
-      fontFamily: 'Public Sans',
-      spaceBetweenItems: 8 // vertical and horizontal, in pixels
-   }
 
-   return <div style={{
-      width: '100vw',
-      height: '100vh'
-   }}>
-      ...
-   </div>
+const defaultProps = {
+  className: "layout",
+  rowHeight: 100,
+};
+
+const removeStyle = {
+  position: "absolute",
+  right: "2px",
+  top: 0,
+  cursor: "pointer",
+};
+
+
+export default function Task3(props) {
+  const DESIGN_DETAILS = {
+    sideBarWidth: 300, // in pixels
+    numberOfInitialBoxes: 69,
+    fontFamily: "Public Sans",
+    spaceBetweenItems: 8, // vertical and horizontal, in pixels
+  };
+  const [items, setItems] = useState(
+    Array.from(Array(50).keys()).map((i, key, list) => {
+      const y = Math.ceil(Math.random() * 4) + 1;
+      return {
+        i: i.toString(),
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6) * y,
+        // w: 2,
+        // h: y,
+        w: _.random(1, 2),
+        h: _.random(1, 3),
+      };
+    })
+  );
+
+  const [newCounter, setCounter] = useState(0);
+  const [gridValue, setGridValue] = useState({
+    breakpoint: null,
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  });
+
+  const createElement = (el) => {
+    const i = el.add ? "+" : el.i;
+    return (
+      <div key={i} data-grid={el}>
+        <span className="text">{i}</span>
+        <span className="remove" style={removeStyle} onClick={onRemoveItem(i)}>
+          x
+        </span>
+      </div>
+    );
+  };
+  
+  const onAddItem = () => {
+    setItems((val) =>
+    
+      items.concat({
+        i: "n" + newCounter,
+        x: (items.length * 2) % 12,
+        y: Infinity,
+        w: _.random(1, 2),
+        h: _.random(1, 3),
+      })
+    );
+    setCounter((val) => val + 1);
+  };
+
+  const onBreakpointChange = (breakpoint, cols) => {
+    setGridValue({ breakpoint, cols });
+  };
+
+  const onRemoveItem = (i) => () => {
+    const updatedItems = _.reject(items, { i: i });
+    setItems(updatedItems);
+  };
+
+  return (
+    <div
+      className="page-container"
+      style={{ fontFamily: DESIGN_DETAILS.fontFamily }}
+    >
+      <div
+        className="sidebar-content"
+        style={{ width: DESIGN_DETAILS.sideBarWidth }}
+      >
+        <button onClick={onAddItem}>Add Item</button>
+      </div>
+
+      <div
+        className="content-content"
+        style={{
+          width: `calc(100% - ${DESIGN_DETAILS.sideBarWidth}px)`,
+          marginLeft: DESIGN_DETAILS.sideBarWidth,
+        }}
+      >
+        <h3>First Page</h3>
+        <ReactGridLayout
+          onBreakpointChange={onBreakpointChange}
+          {...defaultProps}
+          {...props}
+        >
+          {_.map(items, (el) => createElement(el))}
+        </ReactGridLayout>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,57 +12,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Test Job
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/task-1" className={styles.card}>
+            <h2>Task 1 &rarr;</h2>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/task-2" className={styles.card}>
+            <h2>Task 2 &rarr;</h2>
           </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="/task-3"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Task 3 &rarr;</h2>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
